248f70d9d19c63fd8660e3806ff6d826787fad16
Bryson Hotopp <bryson.hotopp@life.church>
2024-10-23 08:35:05 -0500

    Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["@youversion/eslint-config-typescript"],
+  extends: ['@youversion/eslint-config-typescript'],
 };
--- src/slide-5_chain.ts
+++ src/slide-5_chain.ts
@@ -12,31 +12,28 @@
  * `yarn chain`
  */
 
-import { type Post, posts } from "./data";
+import { type Post, posts } from './data';
 
 // Chaining.
-const lastTwoPublishedPostsChained: Array<Omit<Post, "status">> = posts
-  .filter((post) => post?.id && post.status === "published")
-  .map((post) => ({ id: post?.id, title: post?.title }))
+const lastTwoPublishedPostsChained: Array<Omit<Post, 'status'>> = posts
+  .filter(post => post?.id && post.status === 'published')
+  .map(post => ({ id: post?.id, title: post?.title }))
   .slice(0, 2);
 
-console.log("chain");
+console.log('chain');
 console.dir(lastTwoPublishedPostsChained, { depth: null });
 
 // Combine map, filter, and slice
-const lastTwoPublishedPostsReduced = posts.reduce(
-  (publishedPosts: Array<Omit<Post, "status">>, post) => {
-    if (publishedPosts.length < 2 && post?.id && post.status === "published") {
-      publishedPosts.push({
-        id: post.id,
-        title: post.title,
-      });
-    }
+const lastTwoPublishedPostsReduced = posts.reduce((publishedPosts: Array<Omit<Post, 'status'>>, post) => {
+  if (publishedPosts.length < 2 && post?.id && post.status === 'published') {
+    publishedPosts.push({
+      id: post.id,
+      title: post.title,
+    });
+  }
 
-    return publishedPosts;
-  },
-  [],
-);
+  return publishedPosts;
+}, []);
 
-console.log("reduce");
+console.log('reduce');
 console.dir(lastTwoPublishedPostsReduced, { depth: null });
